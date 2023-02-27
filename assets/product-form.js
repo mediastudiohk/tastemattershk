0ab50b011733b1a23219c2b1d8b26f88096353f0
tunv1 <tunv1@vmodev.com>
2023-02-27 10:23:41 +0700

    MSH23020-11 featured product

--- assets/product-form.js
+++ assets/product-form.js
@@ -19,7 +19,7 @@ if (!customElements.get('product-form')) {
 
       this.submitButton.setAttribute('aria-disabled', true);
       this.submitButton.classList.add('loading');
-      this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
+      // this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
 
       const config = fetchConfig('javascript');
       config.headers['X-Requested-With'] = 'XMLHttpRequest';
@@ -70,7 +70,7 @@ if (!customElements.get('product-form')) {
           this.submitButton.classList.remove('loading');
           if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
           if (!this.error) this.submitButton.removeAttribute('aria-disabled');
-          this.querySelector('.loading-overlay__spinner').classList.add('hidden');
+          // this.querySelector('.loading-overlay__spinner').classList.add('hidden');
         });
     }
 
--- assets/quick-add.js
+++ assets/quick-add.js
@@ -17,7 +17,7 @@ if (!customElements.get('quick-add-modal')) {
     show(opener) {
       opener.setAttribute('aria-disabled', true);
       opener.classList.add('loading');
-      opener.querySelector('.loading-overlay__spinner').classList.remove('hidden');
+      // opener.querySelector('.loading-overlay__spinner').classList.remove('hidden');
 
       fetch(opener.getAttribute('data-product-url'))
         .then((response) => response.text())
@@ -42,7 +42,7 @@ if (!customElements.get('quick-add-modal')) {
         .finally(() => {
           opener.removeAttribute('aria-disabled');
           opener.classList.remove('loading');
-          opener.querySelector('.loading-overlay__spinner').classList.add('hidden');
+          // opener.querySelector('.loading-overlay__spinner').classList.add('hidden');
         });
     }
 
